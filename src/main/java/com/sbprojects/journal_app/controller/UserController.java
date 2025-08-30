8ba577a46ec43fe69996f95e911f87a828a2c23f
Swapnil <sb015002@gmail.com>
2025-08-30 17:40:38 +0530

    Adding to repo

--- src/main/java/com/sbprojects/journal_app/controller/UserController.java
+++ src/main/java/com/sbprojects/journal_app/controller/UserController.java
@@ -1,13 +1,8 @@
 package com.sbprojects.journal_app.controller;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -25,26 +20,10 @@ public class UserController {
     @Autowired
     private userService myUserService;
 
-    @GetMapping
-    public List<User> getAll() {
-        return myUserService.getAll();
-    }
-
     @PostMapping
     public void createUser(@RequestBody User myUser) {
         myUserService.saveNewUser(myUser);
     }
-
-    @GetMapping("id/{myId}")
-    public Optional<User> getById(@RequestBody ObjectId myId) {
-        return myUserService.findById(myId);
-    }
-
-    // @DeleteMapping("/{myId}")
-    // public boolean deleteUser(@PathVariable ObjectId myId) {
-    //     myUserService.deleteById(myId);
-    //     return true;
-    // }
 
     @PutMapping("/{oldUser}")
     public ResponseEntity<?> updateUser(@RequestBody User newUser, @PathVariable String oldUser) {
